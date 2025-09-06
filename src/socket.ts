cd9ec81f242d096c12d88e8d07bb8683425a90b8
SSonline <105760309+SmartStudioMedia@users.noreply.github.com>
2025-09-06 14:17:39 +0200

    Update socket.ts

--- src/socket.ts
+++ src/socket.ts
@@ -1,1 +1,8 @@
+import { Server } from "socket.io";
+export let io: Server;
+
+export function attachSocket(httpServer: any) {
+  io = new Server(httpServer, { cors: { origin: "*" } });
+  io.on("connection", () => {/* you can add room logic */});
+}
 
